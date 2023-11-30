29b9929ded91ab93a421f4559fa84e6a874acce8
Enrico Weigelt, metux IT consult <info@metux.net>
2023-11-30 19:36:54 +0100

    util: fileutil: simple dirlisting w/ absolute pathname outputs
    Signed-off-by: Enrico Weigelt, metux IT consult <info@metux.net>

--- util/fileutil/find.go
+++ util/fileutil/find.go
@@ -1,10 +1,25 @@
 package fileutil
 
 import (
+	"os"
+	"strings"
+
 	"github.com/metux/go-metabuild/util/cmd"
 )
 
 func FindFile(dir string, name string) []string {
 	files, _ := cmd.RunOutLines([]string{"find", dir, "-name", name}, true)
 	return files
+}
+
+func ListDir(dir string, suffix string) ([]string, error) {
+	entries, err := os.ReadDir(dir)
+	names := []string{}
+	for _, e := range entries {
+		n := e.Name()
+		if strings.HasSuffix(n, suffix) {
+			names = append(names, dir+"/"+n)
+		}
+	}
+	return names, err
 }
